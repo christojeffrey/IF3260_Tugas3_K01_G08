61527d347939f7cf98b12c2a9eb34430130adf22
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-04-10 14:38:40 +0700

    feat: change cube creation order

--- src/scripts/models/classes/cube.js
+++ src/scripts/models/classes/cube.js
@@ -13,21 +13,28 @@ export class Cube {
     let rectangles = [];
     let points = [];
     // add 8 points
+
+    // back
+    // left
     points.push(new Point(this.center.x - this.width / 2, this.center.y - this.length / 2, this.center.z - this.depth / 2));
     points.push(new Point(this.center.x - this.width / 2, this.center.y + this.length / 2, this.center.z - this.depth / 2));
+    // right
     points.push(new Point(this.center.x + this.width / 2, this.center.y + this.length / 2, this.center.z - this.depth / 2));
     points.push(new Point(this.center.x + this.width / 2, this.center.y - this.length / 2, this.center.z - this.depth / 2));
+    // front
+    // left
     points.push(new Point(this.center.x - this.width / 2, this.center.y - this.length / 2, this.center.z + this.depth / 2));
     points.push(new Point(this.center.x - this.width / 2, this.center.y + this.length / 2, this.center.z + this.depth / 2));
+    // right
     points.push(new Point(this.center.x + this.width / 2, this.center.y + this.length / 2, this.center.z + this.depth / 2));
     points.push(new Point(this.center.x + this.width / 2, this.center.y - this.length / 2, this.center.z + this.depth / 2));
     // add 6 rectangles
     rectangles.push(new Rectangle(points[0], points[1], points[2], points[3]));
-    rectangles.push(new Rectangle(points[4], points[5], points[6], points[7]));
-    rectangles.push(new Rectangle(points[0], points[1], points[5], points[4]));
-    rectangles.push(new Rectangle(points[1], points[2], points[6], points[5]));
-    rectangles.push(new Rectangle(points[2], points[3], points[7], points[6]));
-    rectangles.push(new Rectangle(points[3], points[0], points[4], points[7]));
+    rectangles.push(new Rectangle(points[7], points[6], points[5], points[4]));
+    rectangles.push(new Rectangle(points[4], points[5], points[1], points[0]));
+    rectangles.push(new Rectangle(points[5], points[6], points[2], points[1]));
+    rectangles.push(new Rectangle(points[6], points[7], points[3], points[2]));
+    rectangles.push(new Rectangle(points[7], points[4], points[0], points[3]));
 
     this.points = points;
     this.rectangles = rectangles;
