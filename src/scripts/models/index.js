21a998f99888dbb79628142b27d876028c2b978b
fdavin <fransdavin19@gmail.com>
2023-04-18 16:03:38 +0700

    feat: pig model

--- src/scripts/models/index.js
+++ src/scripts/models/index.js
@@ -1,1 +1,2 @@
 export { elbow } from "./elbow.js";
+export { pig } from "./pig.js";
---
+++ src/scripts/models/pig.js
@@ -0,0 +1,73 @@
+import { Point } from "./classes/point.js";
+import { Model } from "./classes/model.js";
+import { Cube } from "./classes/cube.js";
+
+let pig = new Model("pig");
+
+let firstArm = new Model("firstArm");
+firstArm.setCubes([new Cube(50, 150, 50, new Point(75, 125, 125))]);
+firstArm.completeModelUsingCubes();
+firstArm.setAnchor(new Point(100, 0, 0));
+
+let secondArm = new Model("secondArm");
+secondArm.setCubes([new Cube(50, 150, 50, new Point(-75, 125, 125))]);
+secondArm.completeModelUsingCubes();
+secondArm.setAnchor(new Point(-100, 0, 0));
+
+let thirdArm = new Model("thirdArm");
+thirdArm.setCubes([new Cube(50, 150, 50, new Point(75, 125, -125))]);
+thirdArm.completeModelUsingCubes();
+thirdArm.setAnchor(new Point(100, 0, 0));
+
+let forthArm = new Model("forthArm");
+forthArm.setCubes([new Cube(50, 150, 50, new Point(-75, 125, -125))]);
+forthArm.completeModelUsingCubes();
+forthArm.setAnchor(new Point(-100, 0, 0));
+
+let firstEar = new Model("firstEar"); 
+firstEar.setCubes([new Cube(20, 75, 50, new Point(100, -10, 200))]);
+firstEar.completeModelUsingCubes();
+firstEar.setAnchor(new Point(100, 0, 0));
+
+let secondEar = new Model("secondEar");
+secondEar.setCubes([new Cube(20, 75, 50, new Point(-100, -10, 200))]);
+secondEar.completeModelUsingCubes();
+secondEar.setAnchor(new Point(-100, 0, 0));
+
+let nose = new Model("nose");
+nose.setCubes([new Cube(75, 50, 50, new Point(0, 0, 250))]);
+nose.completeModelUsingCubes();
+nose.setAnchor(new Point(100, 0, 0));
+
+let head = new Model("head");
+head.setCubes([new Cube(175, 125, 150, new Point(0, -12.5, 175))]);
+head.completeModelUsingCubes();
+head.setAnchor(new Point(100, 0, 0));
+head.addChildren("firstEar", firstEar);
+head.updateModelBeingDrawnFully();
+head.addChildren("secondEar", secondEar);
+head.updateModelBeingDrawnFully();
+head.addChildren("nose", nose);
+head.updateModelBeingDrawnFully();
+
+pig.setCubes([new Cube(200, 100, 300, new Point(0, 0, 0))]);
+pig.completeModelUsingCubes();
+
+pig.setAnchor([0, 0, 100]);
+
+pig.addChildren("firstArm", firstArm);
+
+pig.updateModelBeingDrawnFully();
+
+pig.addChildren("head", head);
+pig.updateModelBeingDrawnFully();
+
+pig.addChildren("secondArm", secondArm);
+pig.updateModelBeingDrawnFully();
+
+pig.addChildren("thirdArm", thirdArm);
+pig.updateModelBeingDrawnFully();
+
+pig.addChildren("forthArm", forthArm);
+pig.updateModelBeingDrawnFully();
+export { pig };
