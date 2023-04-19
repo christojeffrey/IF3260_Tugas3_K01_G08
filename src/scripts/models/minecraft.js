7a490dad0b51bf83b0ab633cfed9aaed91ff7498
Christopher Jeffrey <christopherjeffrey492@gmail.com>
2023-04-19 16:08:30 +0700

    feat: finsihing minnnnecraft model

--- src/scripts/models/minecraft.js
+++ src/scripts/models/minecraft.js
@@ -5,17 +5,37 @@ import { Cube } from "./classes/cube.js";
 let minecraft = new Model("elbow");
 
 let head = new Model("head");
-head.setCubes([new Cube(150, 150, 150, new Point(0, -225, 0))]);
+head.setCubes([new Cube(180, 180, 180, new Point(0, -215, 0))]);
 head.completeModelUsingCubes();
 // head.setAnchor(new Point(100, 0, 0));
 
+let leftArm = new Model("leftArm");
+leftArm.setCubes([new Cube(80, 250, 25, new Point(-130, 0, 0))]);
+leftArm.completeModelUsingCubes();
+
+let rightArm = new Model("rightArm");
+rightArm.setCubes([new Cube(80, 250, 25, new Point(130, 0, 0))]);
+rightArm.completeModelUsingCubes();
+
+let leftLeg = new Model("leftLeg");
+leftLeg.setCubes([new Cube(80, 250, 25, new Point(-50, 250, 0))]);
+leftLeg.completeModelUsingCubes();
+
+let rightLeg = new Model("rightLeg");
+rightLeg.setCubes([new Cube(80, 250, 25, new Point(50, 250, 0))]);
+rightLeg.completeModelUsingCubes();
+
 // body
-minecraft.setCubes([new Cube(150, 300, 50, new Point(0, 0, 0))]);
+minecraft.setCubes([new Cube(180, 250, 50, new Point(0, 0, 0))]);
 minecraft.completeModelUsingCubes();
 
 minecraft.setAnchor([0, 0, 100]);
 
 minecraft.addChildren("head", head);
+minecraft.addChildren("leftArm", leftArm);
+minecraft.addChildren("rightArm", rightArm);
+minecraft.addChildren("leftLeg", leftLeg);
+minecraft.addChildren("rightLeg", rightLeg);
 
 minecraft.updateModelBeingDrawnFully();
 export { minecraft };
